f0067c6115a00d9880c4f7f6f4a08248b9dc1570
Princess Habon <princessmaylene53@gmail.com>
2025-09-27 02:06:39 +0800

    Delete old files and update requirements

--- routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import auth, users, dashboard, students, clusters, datasets, reports
-
-# Export routers so main.py can access them directly
-__all__ = ["auth", "users", "dashboard", "students", "clusters", "datasets", "reports"]
