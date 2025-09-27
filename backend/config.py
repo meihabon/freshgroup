492b25560f7c3fb31bca3d5bc1d5249594f4259c
Princess Habon <princessmaylene53@gmail.com>
2025-09-27 18:40:35 +0800

    config

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,15 @@
 import os
 from dotenv import load_dotenv
+from passlib.context import CryptContext
 
 load_dotenv()  # local dev, Railway ignores this
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 1440
+
+# Password hashing
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
