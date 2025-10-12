3b48d05f5bb25bbe12f023680eb85698342265fa
Princess Habon <princessmaylene53@gmail.com>
2025-10-12 14:51:10 +0800

    dataset remp

--- frontend/src/components/DatasetHistory.tsx
+++ frontend/src/components/DatasetHistory.tsx
@@ -211,8 +211,8 @@ function DatasetHistory() {
   const handleDownloadTemplateExcel = () => {
     const header = ["firstname", "lastname", "sex", "program", "municipality", "income", "SHS_type", "GWA"]
     const rows = [
-      ["Juan", "Dela Cruz", "Male", "BSIT", "Tagudin", 15000, "Private", 85],
-      ["Maria", "Santos", "Female", "BSBA", "Sta. Cruz", 8000, "Public", 90],
+      ["Juan", "Dela Cruz", "Male", "BSIT", "TAGDIN", 15000, "Private", 85],
+      ["Maria", "Santos", "Female", "BSBA", "SANTA CRUZ", 8000, "Public", 90],
     ]
 
     const worksheet = XLSX.utils.aoa_to_sheet([header, ...rows])
